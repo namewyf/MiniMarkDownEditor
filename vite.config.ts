5db4c68b8dfa0c0072d5cebb4154955533120a7b
namewyf <wyf19892552132@163.com>
2025-01-18 12:56:11 +0800

    fix:修改base端口+禁用prettier格式化

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MiniMarkDownEditor/', // github仓库名称
   plugins: [
     vue(),
     vueDevTools(),
